3d1b679ec6331e014425b10e3b3d926ead67709c
Alba <dev.mateos.alba@gmail.com>
2021-08-06 12:04:02 +0200

    Putting style together

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import data from '../../shared/links';
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="main-container">
       <ul>
         {
           data.map((link) => (
