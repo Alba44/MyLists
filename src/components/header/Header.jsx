824cf3c52c7b811b53d9bbdd64f98ada8d6f12b6
Alba <dev.mateos.alba@gmail.com>
2021-08-23 15:12:54 +0200

    Implement connect, mapDispatchToProps and prop-types

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,36 @@
 import React, { useState } from 'react';
 import './Header.css';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 import { addLink } from '../../redux/actions/actions';
 
-export default function Header() {
+function Header({ actions }) {
   const [link, setLink] = useState('');
   const [name, setName] = useState('');
 
-  function addNewLink(url, label) {
-    debugger;
-    addLink(url, label);
-  }
   return (
     <header>
       <h1 className="header_title">My Links</h1>
       <form className="header_inputs-container">
         <input className="input-box link" type="text" placeholder="New link" value={link} onChange={(event) => setLink(event.target.value)} />
         <input className="input-box name" type="text" placeholder="Name" value={name} onChange={(event) => setName(event.target.value)} />
-        <button className="add-link" type="button" onClick={addNewLink(link, name)}>Add Link</button>
+        <button className="add-link" type="button" onClick={() => actions.addLink(link, name)}>Add Link</button>
       </form>
     </header>
   );
 }
+
+Header.propTypes = {
+  actions: propTypes.shape({
+    addLink: propTypes.func,
+  }).isRequired,
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({ addLink }, dispatch),
+  };
+}
+
+export default connect(undefined, mapDispatchToProps)(Header);
