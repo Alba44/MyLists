64d9d4cb1ebd293e1e213dc38debe9e2d7eeae4a
Alba <dev.mateos.alba@gmail.com>
2021-08-29 23:35:15 +0200

    Rearrange import order

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,8 +1,8 @@
 import { compose, applyMiddleware, createStore } from 'redux';
 import reduxImmutableStatInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
 import initialState from './initialState';
+import rootReducer from '../reducers';
 
 export default function configureStore() {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
